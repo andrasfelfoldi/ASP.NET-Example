c052a9ec37e4824bb8bdf4073d1b8f094c3ec883
andrasfelfoldi <andrasfelfoldi@gmail.com>
2018-11-05 18:06:36 +0100

    initial model added

---
+++ ASP.NET Example/Models/Movie.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ASP.NET_Example.Models
+{
+    public class Movie
+    {
+        [Required, MinLength(1), MaxLength(50)]
+        public String Title { get; set; }
+        public String ReleaseYear { get; set; }
+        public Rating Rating { get; set; }
+    }
+}
---
+++ ASP.NET Example/Models/Rating.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ASP.NET_Example.Models
+{
+    public enum Rating
+    {
+        Horrible=1,
+        [Display(Name = "Very Bad")]
+        VeryBad,
+        Bad,
+        [Display(Name = "Worse Than Average")]
+        WorseThanAverage,
+        Average,
+        [Display(Name = "Better Than Average")]
+        BetterThanAverage,
+        Good,
+        [Display(Name = "Very Good")]
+        VeryGood,
+        Amazing,
+        Masterpiece
+    }
+}
